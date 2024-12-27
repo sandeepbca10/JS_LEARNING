d9598bd47fa8035dfc13ec1906b7dfcf169e9f04
sandeepbca10 <142310220+sandeepbca10@users.noreply.github.com>
2024-12-27 08:45:32 +0000

    save change

--- 01_basic/02_variable.js
+++ 01_basic/02_variable.js
@@ -1,2 +1,11 @@
 let score = 1254;
 console.log(typeof score);
+
+const func = function myfun() {
+    console.log(2+2);
+}
+
+const url ="https://sandeep.com/sandeep%20yadav"
+
+ console.log( url.replace('%20','-'));
+ 
